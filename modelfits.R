c4648afe6b19a1251a3b877ffa1f95ba2e487158
Robert Link <rplzzz@gmail.com>
2014-04-29 10:44:11 -0400

    Add fits using the lag5 model

--- modelfits.R
+++ modelfits.R
@@ -31,77 +31,98 @@ datasets <- split(master.nonzero, master.nonzero$ISO %in% testing.countries)
 names(datasets) <- c("training", "testing")
 
 ### fit various per-capita cement models
-predictors <- c("urban.growth", "urban.pop", "pcGDP", "GDP.rate", "pccement.stock")
+## formulae to use in the fits:
+##  basic   :  just the state variables
+##  pcstock :  state variables + per-capita stock
+##  lag5    :  state variables + 5-year lagged per-capita production
+##  d1, d2, etc refer to the degree of crossing
+## 
+##  The fitted models are named after the type of the formula used.
+##  We omit the degree unless we fit a couple of models of the same
+##  type and different degree.
+f.basic.d1 <- as.formula(pccement~urban.growth + urban.pop + pcGDP + GDP.rate)
+f.basic.d2 <- as.formula(pccement~(urban.growth + urban.pop + pcGDP + GDP.rate)^2)
+f.pcstock.d1 <- as.formula(pccement~urban.growth + urban.pop + pcGDP + GDP.rate + pccement.stock)
+f.pcstock.d2 <- as.formula(pccement~(urban.growth + urban.pop + pcGDP + GDP.rate + pccement.stock)^2)
+f.lag5.d1 <- as.formula(pccement~urban.growth + urban.pop + pcGDP + GDP.rate + pccement.lag5)
+f.lag5.d2 <- as.formula(pccement~(urban.growth + urban.pop + pcGDP + GDP.rate + pccement.lag5)^2)
+
 ## Linear regression
-pcc.lm <- lm(pccement~(urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock)^2, data=datasets$training)
+pcstock.lm <- lm(formula=f.pcstock.d2, data=datasets$training)
+lag5.lm    <- lm(formula=f.lag5.d2, data=datasets$training)
 
 ## stepwise lr
-pcc.lm.step <- step(pcc.lm)
+pcstock.lm.step <- step(pcstock.lm)
+lag5.lm.step    <- step(lag5.lm)
 
 ## generalized lm (lots more to do here)
-pcc.glm <- glm(formula=pccement~(urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock)^2,
-               family=Gamma(link=log), data=datasets$training)
-
-pcc.glm.gauss <- glm(formula=pccement~(urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock)^2,
-                     family=gaussian(link=log), data=datasets$training)
-
-
-## ## stepwise generalized lm
-## pcc.glm.step <- step(pcc.glm)
+pcstock.glm <- glm(formula=f.pcstock.d2, family=Gamma(link=log), data=datasets$training) 
+pcstock.glm.gauss <- glm(formula=f.pcstock.d2, family=gaussian(link=log), data=datasets$training) 
 
 ## Add some more:  regression trees, etc.
-## regression tree - note regression trees can't do interaction terms
-pcc.rpart <- rpart(formula= pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock, data=datasets$training)
+## regression tree - note regression trees can't do interaction terms, so we have to use the d1 version
+pcstock.rpart <- rpart(formula=f.pcstock.d1, data=datasets$training)
+lag5.rpart    <- rpart(formula=f.lag5.d1, data=datasets$training)
 
 ## random forest
-pcc.rf     <- randomForest(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock, data=datasets$training)
-pcc.rf.int <- randomForest(formula=pccement~(urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock)^2, data=datasets$training)
+pcstock.rf.d1 <- randomForest(formula=f.pcstock.d1, data=datasets$training)
+pcstock.rf.d2 <- randomForest(formula=f.pcstock.d2, data=datasets$training)
 
-## Multi-adaptive regression splines
-pcc.earth <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock, data=datasets$training)
-pcc.earth.d2 <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock,
-                      data=datasets$training, degree=2, nk=64)
-pcc.earth.d2a <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock,
-                      data=datasets$training, degree=2, nk=128)
-pcc.earth.d2b <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock,
-                       data=datasets$training, degree=2, nk=128, nprune=15)
-pcc.earth.d3 <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock,
-                      data=datasets$training, degree=3, nk=128)
-pcc.earth.d3a <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate+pccement.stock,
-                      data=datasets$training, degree=3, nk=128, nprune=20)
-pcc.earth.nostock <- earth(formula=pccement~urban.growth+urban.pop+pcGDP+GDP.rate,
-                           data=datasets$training, degree=3, nk=128, nprune=15)
+## Multi-adaptive regression splines 
+## Note: the earth function always wants a degree=1 formula.  You
+##       specify the crossing degree explicitly with an argument to
+##       the fitting function. 
+## TODO There is a lot more we can do here.  Notably, we could use the glm capability to try to fit log(pccement)
+pcstock.earth.d1  <- earth(formula=f.pcstock.d1, data=datasets$training)
+pcstock.earth.d2  <- earth(formula=f.pcstock.d1, data=datasets$training, degree=2, nk=64)
+pcstock.earth.d2a <- earth(formula=f.pcstock.d1, data=datasets$training, degree=2, nk=128)
+pcstock.earth.d2b <- earth(formula=f.pcstock.d1, data=datasets$training, degree=2, nk=128, nprune=15)
+pcstock.earth.d3  <- earth(formula=f.pcstock.d1, data=datasets$training, degree=3, nk=128)
+pcstock.earth.d3a <- earth(formula=f.pcstock.d1, data=datasets$training, degree=3, nk=128, nprune=20)
 
+basic.earth.d3    <- earth(formula=f.basic.d1, data=datasets$training, degree=3, nk=128, nprune=15)
+
+lag5.earth.d1     <- earth(formula=f.lag5.d1, data=datasets$training, degree=1)
+lag5.earth.d2     <- earth(formula=f.lag5.d1, data=datasets$training, degree=2, nk=128)
+lag5.earth.d2a    <- earth(formula=f.lag5.d1, data=datasets$training, degree=2, nk=128, nprune=15)
+lag5.earth.d3     <- earth(formula=f.lag5.d1, data=datasets$training, degree=3, nk=128)
+lag5.earth.d3a    <- earth(formula=f.lag5.d1, data=datasets$training, degree=3, nk=128, nprune=20)
 
 source("modelanly.R")
 
-cat("\nlinear:\t\tpcc.lm\n")
-print(rms.eval(pcc.lm, datasets, FALSE))
-cat("\nstepwise linear:\t\tpcc.lm.step\n")
-print(rms.eval(pcc.lm.step, datasets, FALSE))
-cat("\nglm (Gamma(link=log)):\t\tpcc.glm\n")
-print(rms.eval(pcc.glm, datasets, FALSE))
-## cat("\nstepwise glm:\n") ### didn't solve properly
-## print(rms.eval(pcc.glm.step, datasets, FALSE))
-cat("\nglm (gaussian(link=log)):\t\tpcc.glm.gauss\n")
-print(rms.eval(pcc.glm.gauss, datasets, FALSE))
-cat("\nregression tree:\t\tpcc.rpart\n")
-print(rms.eval(pcc.rpart, datasets, FALSE))
-cat("\nrandom forest (no interactions):\t\tpcc.rf\n")
-print(rms.eval(pcc.rf, datasets, FALSE))
-cat("\nrandom forest (including interaction terms):\t\tprr.rf.int\n")
-print(rms.eval(pcc.rf.int, datasets, FALSE))
-cat("\nMARS degree=1 (i.e., no interactions):\t\tpcc.earth\n")
-print(rms.eval(pcc.earth, datasets, FALSE))
-cat("\nMARS degree=2 (first-order interactions):\t\tpcc.earth.d2\n")
-print(rms.eval(pcc.earth.d2, datasets, FALSE))
-cat("\nMARS degree=2, nk=128:\t\tpcc.earth.d2a\n")
-print(rms.eval(pcc.earth.d2a, datasets, FALSE))
-cat("\nMARS degree=2, pruned to 15 terms:\t\tpcc.earth.d2b\n")
-print(rms.eval(pcc.earth.d2b, datasets, FALSE))
-cat("\nMARS degree=3 (second-order interactions):\t\tpcc.earth.d3\n")
-print(rms.eval(pcc.earth.d3, datasets, FALSE))
-cat("\nMARS degree=3, pruned to 20 terms:\t\tpcc.earth.d3a\n")
-print(rms.eval(pcc.earth.d3a, datasets, FALSE))
-cat("\nMARS degree=3, no carbon stock term:\t\tpcc.earth.nostock\n")
-print(rms.eval(pcc.earth.nostock, datasets, FALSE))
+cat("\nlinear:\t\tpcstock.lm\n")
+print(rms.eval(pcstock.lm, datasets, FALSE))
+cat("\nstepwise linear:\t\tpcstock.lm.step\n")
+print(rms.eval(pcstock.lm.step, datasets, FALSE))
+cat("\nstepwise linear lag5:\t\tlag5.lm.step\n")
+print(rms.eval(lag5.lm.step, datasets, FALSE))
+cat("\nglm (Gamma(link=log)):\t\tpcstock.glm\n")
+print(rms.eval(pcstock.glm, datasets, FALSE))
+cat("\nglm (gaussian(link=log)):\t\tpcstock.glm.gauss\n")
+print(rms.eval(pcstock.glm.gauss, datasets, FALSE))
+cat("\nregression tree:\t\tpcstock.rpart\n")
+print(rms.eval(pcstock.rpart, datasets, FALSE))
+cat("\nregression tree lag5:\t\tlag5.rpart\n")
+print(rms.eval(lag5.rpart, datasets, FALSE))
+cat("\nrandom forest (no interactions):\t\tpcstock.rf.d1\n")
+print(rms.eval(pcstock.rf.d1, datasets, FALSE))
+cat("\nrandom forest (including interaction terms):\t\tpcstock.rf.d2\n")
+print(rms.eval(pcstock.rf.d2, datasets, FALSE))
+cat("\nMARS degree=1 (i.e., no interactions):\t\tpcstock.earth.d1\n")
+print(rms.eval(pcstock.earth.d1, datasets, FALSE))
+cat("\nMARS degree=2, pruned to 15 terms:\t\tpcstock.earth.d2b\n")
+print(rms.eval(pcstock.earth.d2b, datasets, FALSE))
+cat("\nMARS degree=3, pruned to 20 terms:\t\tpcstock.earth.d3a\n")
+print(rms.eval(pcstock.earth.d3a, datasets, FALSE))
+cat("\nMARS degree=3, no carbon stock term:\t\tbasic.earth.d3\n")
+print(rms.eval(basic.earth.d3, datasets, FALSE))
+cat("\nMARS degree=1, lag5:\t\tlag5.earth.d1\n")
+print(rms.eval(lag5.earth.d1, datasets, FALSE)) 
+cat("\nMARS degree=2, lag5:\t\tlag5.earth.d2\n")
+print(rms.eval(lag5.earth.d2, datasets, FALSE))
+cat("\nMARS degree=2, lag5 (pruned to 15):\t\tlag5.earth.d2a\n")
+print(rms.eval(lag5.earth.d2a, datasets, FALSE))
+cat("\nMARS degree=3, lag5:\t\tlag5.earth.d3\n")
+print(rms.eval(lag5.earth.d3, datasets, FALSE))
+cat("\nMARS degree=3, lag5 (pruned to 20):\t\tlag5.earth.d3a\n")
+print(rms.eval(lag5.earth.d3a, datasets, FALSE))
