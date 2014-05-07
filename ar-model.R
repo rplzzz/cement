6b0ebba2c49527b9fa2768dd4bf582f885202ae5
Robert Link <rplzzz@gmail.com>
2014-05-07 10:54:53 -0400

    Finish the autoregression model
    Still doesn't work well.  Doesn't solve in optim with default options.
    The way that we handle zeros in predictions or actuals is kind of
    messed up.

--- ar-model.R
+++ ar-model.R
@@ -26,7 +26,7 @@ loglik.ar <- function(coefs) {
                        return(0)
                    
                    yhat           <- rep(0,n)
-                   yhat[1:ar.lag] <- coefs %*% t(as.matrix(cbind(tbl[1:ar.lag,columns.armod], 1)))
+                   yhat[1:ar.lag] <- tcrossprod(coefs, as.matrix(cbind(tbl[1:ar.lag,columns.armod], 1)))
                    for(i in (ar.lag+1):nrow(tbl)) {
                        x       <- cbind(tbl[i,columns.armod], 1)
                        x[1]    <- yhat[i-ar.lag]     # the previous value of yhat
@@ -37,11 +37,12 @@ loglik.ar <- function(coefs) {
 }
 
 loglik.ar.single <- function(yhat, y) {
-    ifelse(yhat > 1.0e-6,
-           dweibull(y, shape=2, scale=yhat*sqrt(2), log=TRUE),
-           ## if yhat is tiny or negative, clamp the prediction to
-           ## something smaller than the smallest actual value, then add
-           ## a linear penalty
-           dweibull(y, shape=2, scale=1.0e-6*sqrt(2), log=TRUE) - (1.0e-6 - yhat))
+    yhat <- max(yhat, 0)
+    ifelse(yhat==0,
+           ifelse(y==0, 0,               # match: zero predicted, zero actual
+                  dlnorm(y, meanlog=1.0e-6, sdlog=0.5, log=TRUE) - (y-yhat)),
+           ifelse(y==0,
+                  dlnorm(yhat, meanlog=1.0e-6, sdlog=0.5, log=TRUE) - (yhat-y),
+                  dlnorm(y, meanlog=yhat, sdlog=0.5, log=TRUE)))
 }
 
